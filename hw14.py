d3424de34769fcd9285ddf45ba229170f0ab4150
michaelmitus <49987038+michaelmitus@users.noreply.github.com>
2019-05-19 14:24:45 +0500

    Add files via upload

---
+++ hw14.py
@@ -0,0 +1,90 @@
+import sqlite3 as lite
+import sys
+import pprint
+import pickle
+
+def first():
+    # Вывести 10 клиентов (id, имя, номер телефона, компания),
+    # которых обслужлуживают сотрудники старше 50 лет,
+    # оплативших музыку в любом жанре кроме Rock,
+    # выходные данные должны быть отсортированы по городу пользователя в алфавитном порядке и емейлу в обратном.
+    try:
+        con = None
+        con = lite.connect('Chinook_Sqlite.sqlite')
+        query_invoice = '''
+              SELECT Customer.CustomerID, Customer.FirstName, Customer.LastName, Customer.Phone, Customer.Company  
+              FROM Customer 
+              LEFT JOIN Employee ON Customer.SupportRepId = Employee.EmployeeId
+              LEFT JOIN Invoice ON Customer.CustomerID = Invoice.CustomerID   
+              LEFT JOIN InvoiceLine ON InvoiceLine.InvoiceID = Invoice.InvoiceID    
+              LEFT JOIN Track ON InvoiceLine.TrackID = Track.TrackID  
+              LEFT JOIN Genre ON Track.GenreId = Genre.GenreId
+              WHERE Genre.Name NOT LIKE 'Rock' AND Employee.BirthDate < "1969-05-19"
+              ORDER BY Employee.City ASC, Employee.Email DESC
+              -- LIMIT = 10    
+            '''
+        curID = con.cursor()
+        curID.execute(query_invoice)
+        pprint.pprint(curID.fetchall())
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+    finally:
+        if con is not None:
+            con.close()
+
+def second():
+    # Вывести список пользователей (полное имя, телефон) с указанием руководителя (полное имя, телефон), сохранить в pickle в формате словаря.
+    try:
+        con = None
+        con = lite.connect('Chinook_Sqlite.sqlite')
+        query_invoice = '''
+              SELECT e.FirstName, e.LastName, e.Phone, m.FirstName, m.LastName, m.Phone  FROM Employee e, Employee m 
+              WHERE m.EmployeeID = e.ReportsTo 
+            '''
+        curID = con.cursor()
+        curID.execute(query_invoice)
+        pickle_data = curID.fetchall()
+        pprint.pprint(pickle_data)                     #Вывести список пользователей (полное имя, телефон) с указанием руководителя (полное имя, телефон)
+        pickle_file = open('pickle.pickle','wb')
+        pickle.dump(pickle_data, pickle_file)           # сохранить в pickle
+        pickle_file.close()
+    except Exception as e:
+      print(e)
+      sys.exit(1)
+    finally:
+      if con is not None:
+        con.close()
+
+def thrid():
+    # Вывести отсортированный список клиентов (имя, телефон) оплативших самые дорогие музыкальные треки.
+    try:
+        con = None
+        con = lite.connect('Chinook_Sqlite.sqlite')
+        query_invoice = '''
+              SELECT Customer.FirstName, Customer.LastName, Customer.Phone, InvoiceLine.UnitPrice
+              FROM Customer 
+              LEFT JOIN Invoice ON Customer.CustomerID = Invoice.CustomerID   
+              LEFT JOIN InvoiceLine ON InvoiceLine.InvoiceID = Invoice.InvoiceID    
+              WHERE InvoiceLine.UnitPrice = (SELECT max(InvoiceLine.UnitPrice) FROM InvoiceLine)
+              ORDER BY Customer.FirstName
+            '''
+        curID = con.cursor()
+        curID.execute(query_invoice)
+        pprint.pprint(curID.fetchall())
+    except Exception as e:
+      print(e)
+      sys.exit(1)
+    finally:
+      if con is not None:
+        con.close()
+
+#Вывести 10 клиентов (id, имя, номер телефона, компания), которых обслужлуживают сотрудники старше 50 лет, оплативших музыку в любом жанре кроме Rock, выходные данные должны быть отсортированы по городу пользователя в алфавитном порядке и емейлу в обратном.
+first()
+
+#Вывести список пользователей (полное имя, телефон) с указанием руководителя (полное имя, телефон), сохранить в pickle в формате словаря.
+#second()
+
+#Вывести отсортированный список клиентов (имя, телефон) оплативших самые дорогие музыкальные треки.
+#thrid()
+
