665c3a581e4b9c7fa2d64b51709bc3ff8d96aaf8
michaelmitus <49987038+michaelmitus@users.noreply.github.com>
2019-05-19 18:34:07 +0500

    Add files via upload

--- hw14.py
+++ hw14.py
@@ -3,7 +3,69 @@ import sys
 import pprint
 import pickle
 
-def first():
+def first():               # Рабочий вариант через вложенные запросы
+    # Вывести 10 клиентов (id, имя, номер телефона, компания),
+    # которых обслужлуживают сотрудники старше 50 лет,
+    # оплативших музыку в любом жанре кроме Rock,
+    # выходные данные должны быть отсортированы по городу пользователя в алфавитном порядке и емейлу в обратном.
+    try:
+        con = None
+        con = lite.connect('Chinook_Sqlite.sqlite')
+        query_invoice = '''
+              SELECT Customer.CustomerID, Customer.FirstName, Customer.LastName, Customer.Phone, Customer.Company
+              FROM Customer 
+              INNER JOIN Employee ON Customer.SupportRepId = Employee.EmployeeId
+              WHERE Employee.BirthDate < "1969-05-19" AND 
+              EXISTS (SELECT Genre.Name FROM Genre
+                  INNER JOIN Track ON Track.GenreId = Genre.GenreId
+                  INNER JOIN InvoiceLine ON InvoiceLine.TrackID = Track.TrackID  
+                  INNER JOIN Invoice ON InvoiceLine.InvoiceID = Invoice.InvoiceID    
+                  INNER JOIN Customer ON Customer.CustomerID = Invoice.CustomerID   
+                  WHERE Genre.Name NOT LIKE 'Rock') 
+              ORDER BY Employee.City ASC, Employee.Email DESC
+              LIMIT 10    
+            '''
+        curID = con.cursor()
+        curID.execute(query_invoice)
+        pprint.pprint(curID.fetchall())
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+    finally:
+        if con is not None:
+            con.close()
+
+def first2():               # Нерабочий вариант
+    # Вывести 10 клиентов (id, имя, номер телефона, компания),
+    # которых обслужлуживают сотрудники старше 50 лет,
+    # оплативших музыку в любом жанре кроме Rock,
+    # выходные данные должны быть отсортированы по городу пользователя в алфавитном порядке и емейлу в обратном.
+    try:
+        con = None
+        con = lite.connect('Chinook_Sqlite.sqlite')
+        query_invoice = '''
+              SELECT Customer.CustomerID, Customer.FirstName, Customer.LastName, Customer.Phone, Customer.Company
+              FROM Customer 
+              INNER JOIN Employee ON Customer.SupportRepId = Employee.EmployeeId
+              INNER JOIN Invoice ON Customer.CustomerID = Invoice.CustomerID   
+              INNER JOIN InvoiceLine ON InvoiceLine.InvoiceID = Invoice.InvoiceID    
+              INNER JOIN Track ON InvoiceLine.TrackID = Track.TrackID  
+              INNER JOIN Genre ON Track.GenreId = Genre.GenreId
+              WHERE Genre.Name NOT LIKE 'Rock' AND Employee.BirthDate < "1969-05-19"
+              ORDER BY Employee.City ASC, Employee.Email DESC
+              -- LIMIT = 10    
+            '''
+        curID = con.cursor()
+        curID.execute(query_invoice)
+        pprint.pprint(curID.fetchall())
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+    finally:
+        if con is not None:
+            con.close()
+
+def first_crutch():             # Без вложенных запросов, но через костыли
     # Вывести 10 клиентов (id, имя, номер телефона, компания),
     # которых обслужлуживают сотрудники старше 50 лет,
     # оплативших музыку в любом жанре кроме Rock,
@@ -25,7 +87,19 @@ def first():
             '''
         curID = con.cursor()
         curID.execute(query_invoice)
-        pprint.pprint(curID.fetchall())
+        list_of_all = curID.fetchall()
+        good_list = []
+        counter = 0
+        for iterator in list_of_all:
+            if iterator in good_list:
+                pass
+            else:
+                good_list.append(iterator)
+                counter += 1
+            if counter >= 10:
+                break
+        pprint.pprint(good_list)
+
     except Exception as e:
         print(e)
         sys.exit(1)
@@ -33,7 +107,7 @@ def first():
         if con is not None:
             con.close()
 
-def second():
+def second():                   # Рабочий вариант
     # Вывести список пользователей (полное имя, телефон) с указанием руководителя (полное имя, телефон), сохранить в pickle в формате словаря.
     try:
         con = None
@@ -56,13 +130,38 @@ def second():
       if con is not None:
         con.close()
 
-def thrid():
+def third():                # Рабочий вариант через вложенные запросы
     # Вывести отсортированный список клиентов (имя, телефон) оплативших самые дорогие музыкальные треки.
     try:
         con = None
         con = lite.connect('Chinook_Sqlite.sqlite')
         query_invoice = '''
-              SELECT Customer.FirstName, Customer.LastName, Customer.Phone, InvoiceLine.UnitPrice
+              SELECT Customer.FirstName, Customer.LastName, Customer.Phone
+              FROM Customer
+              WHERE EXISTS 
+                (SELECT InvoiceLine.UnitPrice from InvoiceLine
+                 INNER JOIN Invoice ON InvoiceLine.InvoiceID = Invoice.InvoiceID    
+                 INNER JOIN Customer ON Customer.CustomerID = Invoice.CustomerID   
+                 WHERE InvoiceLine.UnitPrice = (SELECT max(InvoiceLine.UnitPrice) FROM InvoiceLine)) 
+              ORDER BY Customer.FirstName
+            '''
+        curID = con.cursor()
+        curID.execute(query_invoice)
+        pprint.pprint(curID.fetchall())
+    except Exception as e:
+      print(e)
+      sys.exit(1)
+    finally:
+      if con is not None:
+        con.close()
+
+def third2():               # Нерабочий вариант
+    # Вывести отсортированный список клиентов (имя, телефон) оплативших самые дорогие музыкальные треки.
+    try:
+        con = None
+        con = lite.connect('Chinook_Sqlite.sqlite')
+        query_invoice = '''
+              SELECT Customer.FirstName, Customer.LastName, Customer.Phone
               FROM Customer 
               LEFT JOIN Invoice ON Customer.CustomerID = Invoice.CustomerID   
               LEFT JOIN InvoiceLine ON InvoiceLine.InvoiceID = Invoice.InvoiceID    
@@ -79,12 +178,47 @@ def thrid():
       if con is not None:
         con.close()
 
+def third_crutch():                 # Рабочий вариант без вложенных запросов но через костыль
+    # Вывести отсортированный список клиентов (имя, телефон) оплативших самые дорогие музыкальные треки.
+    try:
+        con = None
+        con = lite.connect('Chinook_Sqlite.sqlite')
+        query_invoice = '''
+              SELECT Customer.FirstName, Customer.LastName, Customer.Phone
+              FROM Customer 
+              LEFT JOIN Invoice ON Customer.CustomerID = Invoice.CustomerID   
+              LEFT JOIN InvoiceLine ON InvoiceLine.InvoiceID = Invoice.InvoiceID    
+              WHERE InvoiceLine.UnitPrice = (SELECT max(InvoiceLine.UnitPrice) FROM InvoiceLine)
+              ORDER BY Customer.FirstName
+            '''
+        curID = con.cursor()
+        curID.execute(query_invoice)
+        list_of_all = curID.fetchall()
+        good_list = []
+        counter = 0
+        for iterator in list_of_all:
+            if iterator in good_list:
+                pass
+            else:
+                good_list.append(iterator)
+                counter += 1
+            if counter >= 100:
+                break
+        pprint.pprint(good_list)
+    except Exception as e:
+      print(e)
+      sys.exit(1)
+    finally:
+      if con is not None:
+        con.close()
+
 #Вывести 10 клиентов (id, имя, номер телефона, компания), которых обслужлуживают сотрудники старше 50 лет, оплативших музыку в любом жанре кроме Rock, выходные данные должны быть отсортированы по городу пользователя в алфавитном порядке и емейлу в обратном.
-first()
+first()                                # работает
+#first_crutch()                         # работает верно, но с костылём
 
 #Вывести список пользователей (полное имя, телефон) с указанием руководителя (полное имя, телефон), сохранить в pickle в формате словаря.
-#second()
+second()
 
 #Вывести отсортированный список клиентов (имя, телефон) оплативших самые дорогие музыкальные треки.
-#thrid()
-
+third()                                # работает
+#third_crutch()                          # работает верно, но с костылём
